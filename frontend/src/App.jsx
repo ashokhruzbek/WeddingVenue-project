54c2f66dc56b1c779fcaeb41f7dd138391edb0da
shokhruzbek <ashokhruzbek@gmail.com>
2025-05-11 13:52:34 +0500

    some updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import "./App.css"
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Login/>}></Route>
-      <Route path='/signup' element={<SignUp/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
