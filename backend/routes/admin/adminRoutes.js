60b729092a0a8dddbc29181edcca6791f3484c5c
shokhruzbek <ashokhruzbek@gmail.com>
2025-05-14 18:58:31 +0500

    added singleVenue

--- backend/routes/admin/adminRoutes.js
+++ backend/routes/admin/adminRoutes.js
@@ -9,6 +9,7 @@ const { createOwner } = require('../../controllers/admin/createOwner');
 const { deleteVenue } = require('../../controllers/admin/deleteVenue');
 const { viewAllVenues } = require('../../controllers/admin/view-all-Venues');
 const { viewFilteredVenues } = require('../../controllers/admin/viewFilteredVenues');
+const { getSingleVenue } = require('../../controllers/admin/singleVenue');
 
 // adminRouter.use(checkRole(['admin']))
 
@@ -19,5 +20,6 @@ adminRouter.post('/create-owner', createOwner)
 adminRouter.delete('/delete-venue/:id', deleteVenue)
 adminRouter.get('/view-all-venues', authentication, viewAllVenues)
 adminRouter.get('/venues', authentication, viewFilteredVenues)
+adminRouter.get('/get-single-venue/:id',authentication, getSingleVenue)
 
 module.exports = adminRouter;
